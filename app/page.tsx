85d9c515937ad2eee2d002da7306bc414c543a9b
abhithele-one <abhishek.thele@truscholar.io>
2025-02-24 18:29:10 +0530

    Added Theme2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import DealerHome from "./component/dealer-home";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+  useEffect(() => {
+  setTheme('dark');
+ 
+  }, [])
+  
   return (
   <div>
     <DealerHome />
