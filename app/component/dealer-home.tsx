b9468007f18d925babe99ef743832da50ec5388e
abhithele-one <abhishek.thele@truscholar.io>
2025-02-24 16:25:32 +0530

    error removed

--- app/component/dealer-home.tsx
+++ app/component/dealer-home.tsx
@@ -1,4 +1,4 @@
-// import { Input } from "@/components/ui/input";
+"use client"
 
 import DealerCard from "./dealer-card";
 
@@ -38,21 +38,19 @@ const DealerHome = () => {
       </div>
       <div className="text-3xl px-8 py-2">
         <p>Manage your Inventory with ease! 🚀</p>
-        {/* <Input className="my-4" /> */}
-        {/* <InventoryCard title="View Assigned Categories" img="https://img.freepik.com/free-photo/mid-century-modern-living-room-interior-design-with-monstera-tree_53876-129805.jpg" count="25" /> */}
         </div>
       <div className="flex justify-between px-4">
         <DealerCard
           count="110"
           title1="Total"
           title2="Inventory"
-          icon="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM2MTVFODMiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1ub3RlYm9vay10YWJzIj48cGF0aCBkPSJNMiA2aDQiLz48cGF0aCBkPSJNMiAxMGg0Ii8+PHBhdGggZD0iTTIgMTRoNCIvPjxwYXRoIGQ9Ik0yIDE4aDQiLz48cmVjdCB3aWR0aD0iMTYiIGhlaWdodD0iMjAiIHg9IjQiIHk9IjIiIHJ4PSIyIi8+PHBhdGggZD0iTTE1IDJ2MjAiLz48cGF0aCBkPSJNMTUgN2g1Ii8+PHBhdGggZD0iTTE1IDEyaDUiLz48cGF0aCBkPSJNMTUgMTdoNSIvPjwvc3ZnPg=="
+         
         />
         <DealerCard
           count="110"
           title1="Sold"
           title2="Items"
-          icon="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM2MTVFODMiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1ib29rLXVwLTIiPjxwYXRoIGQ9Ik0xMiAxM1Y3Ii8+PHBhdGggZD0iTTE4IDJoMWExIDEgMCAwIDEgMSAxdjE4YTEgMSAwIDAgMS0xIDFINi41YTEgMSAwIDAgMSAwLTVIMjAiLz48cGF0aCBkPSJNNCAxOS41di0xNUEyLjUgMi41IDAgMCAxIDYuNSAyIi8+PHBhdGggZD0ibTkgMTAgMy0zIDMgMyIvPjxwYXRoIGQ9Im05IDUgMy0zIDMgMyIvPjwvc3ZnPg=="
+
         />
       </div>
       <div className="flex justify-between px-4">
@@ -60,13 +58,13 @@ const DealerHome = () => {
           count="110"
           title1="Pending"
           title2="Issues"
-          icon="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM2MTVFODMiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1zaGllbGQtY2hlY2siPjxwYXRoIGQ9Ik0yMCAxM2MwIDUtMy41IDcuNS03LjY2IDguOTVhMSAxIDAgMCAxLS42Ny0uMDFDNy41IDIwLjUgNCAxOCA0IDEzVjZhMSAxIDAgMCAxIDEtMWMyIDAgNC41LTEuMiA2LjI0LTIuNzJhMS4xNyAxLjE3IDAgMCAxIDEuNTIgMEMxNC41MSAzLjgxIDE3IDUgMTkgNWExIDEgMCAwIDEgMSAxeiIvPjxwYXRoIGQ9Im05IDEyIDIgMiA0LTQiLz48L3N2Zz4="
+
         />
         <DealerCard
           count="110"
           title1="Active"
           title2="Warranty"
-          icon="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM2MTVFODMiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1sb2FkZXIiPjxwYXRoIGQ9Ik0xMiAydjQiLz48cGF0aCBkPSJtMTYuMiA3LjggMi45LTIuOSIvPjxwYXRoIGQ9Ik0xOCAxMmg0Ii8+PHBhdGggZD0ibTE2LjIgMTYuMiAyLjkgMi45Ii8+PHBhdGggZD0iTTEyIDE4djQiLz48cGF0aCBkPSJtNC45IDE5LjEgMi45LTIuOSIvPjxwYXRoIGQ9Ik0yIDEyaDQiLz48cGF0aCBkPSJtNC45IDQuOSAyLjkgMi45Ii8+PC9zdmc+"
+
         />
       
       </div>
